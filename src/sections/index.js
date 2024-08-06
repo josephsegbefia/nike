72a6763590ccaeecaaeb6f7e5a1cbc94e3822ae9
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-08-06 11:08:53 +0200

    Complete

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import SuperQuality from './SuperQuality';
+import Subscribe from './Subscribe';
+import Services from './Services';
+import PopularProducts from './PopularProducts';
+import SpecialOffer from './SpecialOffer';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+
+export {
+  Hero,
+  SuperQuality,
+  Subscribe,
+  Services,
+  PopularProducts,
+  SpecialOffer,
+  CustomerReviews,
+  Footer,
+};
